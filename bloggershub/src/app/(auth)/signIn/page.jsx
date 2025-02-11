e700a6213d5e3e1650296f88aa4af0f601be0e2b
Rajesh <rajeshranjan8271@gmail.com>
2025-02-12 02:48:03 +0530

    Added form to tak inspiration from

--- bloggershub/src/app/(auth)/signIn/page.jsx
+++ bloggershub/src/app/(auth)/signIn/page.jsx
@@ -1,7 +1,19 @@
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
 const SignIn = () => {
   return (
     <section className="flex justify-center items-center px-10 w-screen h-screen">
-      Sign In Page
+      <div className="border-2 border-black rounded-lg min-w-96 min-h-96">
+        <h1>Sign In</h1>
+      </div>
     </section>
   );
 };
