980687a04f8b00c7da4ffd2425b14d4a1fff5c9e
Rajesh <rajeshranjan8271@gmail.com>
2025-02-13 18:21:27 +0530

    Blog post cards alignment

--- bloggershub/src/app/blog/page.jsx
+++ bloggershub/src/app/blog/page.jsx
@@ -4,10 +4,10 @@ const Blog = () => {
   return (
     <section className="flex justify-center pt-16 w-screen">
       <div className="flex flex-col justify-center px-10">
-        <div className="p-5 border-b-4 border-blue-400 w-full xl:w-1/3 font-semibold text-3xl xl:text-left text-center">
+        <div className="p-5 border-b-4 border-blue-400 w-full md:w-1/3 xl:w-1/6 font-semibold text-3xl xl:text-left text-center">
           Latest Posts
         </div>
-        <div className="flex md:flex-row flex-col flex-wrap justify-center gap-10 py-10 w-full">
+        <div className="justify-between gap-10 grid md:grid-cols-2 xl:grid-cols-4 py-10 w-full">
           <BlogCard />
           <BlogCard />
           <BlogCard />
