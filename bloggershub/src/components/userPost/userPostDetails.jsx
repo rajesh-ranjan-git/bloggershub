b8f2aaf078b7ef7e9e4abe32828f1bc1f1a92c34
Rajesh <rajeshranjan8271@gmail.com>
2025-02-14 02:46:09 +0530

    Delete posat UI

--- bloggershub/src/components/userPost/userPostDetails.jsx
+++ bloggershub/src/components/userPost/userPostDetails.jsx
@@ -1,5 +1,16 @@
 import { Button } from "../ui/button";
 import { MdDelete, MdModeEdit } from "react-icons/md";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const UserPostDetails = () => {
   return (
@@ -17,12 +28,32 @@ const UserPostDetails = () => {
         >
           <MdModeEdit />
         </Button>
-        <Button
-          variant="outline"
-          className="hover:bg-red-600 p-0 border-red-600 w-10 text-red-600 hover:text-white"
-        >
-          <MdDelete />
-        </Button>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button
+              variant="outline"
+              className="hover:bg-red-600 p-0 border-red-600 w-10 text-red-600 hover:text-white"
+            >
+              <MdDelete />
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent className="rounded-lg w-[90vw] md:w-auto">
+            <AlertDialogHeader>
+              <AlertDialogTitle>Permanently delete this post?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                post from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction className="flex items-center gap-2 bg-red-600 hover:bg-red-700 border-red-600 hover:border-red-700 text-white h">
+                <MdDelete />
+                <span>Delete</span>
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     </div>
   );
