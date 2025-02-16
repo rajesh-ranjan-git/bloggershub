b0d88fe636c39fd76ac9bc91abed95301bb2c4d7
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-02-16 22:24:43 +0530

    Updated customInput and label

--- bloggershub/src/components/customFormElements/customFileInput.jsx
+++ bloggershub/src/components/customFormElements/customFileInput.jsx
@@ -25,11 +25,11 @@ const CustomFileInput = ({
             <Input
               type="file"
               placeholder={placeholder}
-              className="shadow-md focus-visible:shadow-md focus-visible:ring-blue-400 w-full focus-visible:placeholder:text-muted-foreground placeholder:text-transparent emailInput"
+              className="shadow-md focus-visible:shadow-md focus-visible:ring-blue-400 w-full focus-visible:placeholder:text-muted-foreground placeholder:text-transparent customInput"
               {...field}
             />
           </FormControl>
-          <FormLabel className="top-[1.5em] left-[1.5em] absolute border-2 border-transparent text-md transition-all -translate-y-[50%] duration-300 ease-in-out pointer-events-none emailLabel transform">
+          <FormLabel className="top-[1.5em] left-[1.5em] absolute border-2 border-transparent text-md transition-all -translate-y-[50%] duration-300 ease-in-out pointer-events-none customLabel transform">
             {label}
           </FormLabel>
           <FormMessage />
