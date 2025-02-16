277822ada82513cc2fae3f86a33dfb91232a471a
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-02-17 01:52:44 +0530

    authmiddleware and checkauth

---
+++ bloggershubbackend/middlewares/auth/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+
+// Auth middleware
+const authMiddleware = async (req, res, next) => {
+  // Get token from cookies
+  const token = req.cookies.token;
+
+  // For setting header for localStorage
+  // const authHeader = req.headers["authorization"];
+  // const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token)
+    return res.json({
+      status: 401,
+      success: false,
+      message: "Unauthorized user!",
+    });
+
+  try {
+    // Decoding token to get user
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    return res.json({
+      status: 401,
+      success: false,
+      message: "Unauthorized user!",
+    });
+  }
+};
+
+export default authMiddleware;
--- bloggershubbackend/router/auth/authRouter.js
+++ bloggershubbackend/router/auth/authRouter.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import signUp from "../../controllers/auth/signUpController.js";
 import signIn from "../../controllers/auth/signInController.js";
+import signOut from "../../controllers/auth/signOutController.js";
+import checkAuth from "../../controllers/auth/checkAuthController.js";
+import authMiddleware from "../../middlewares/auth/authMiddleware.js";
 
 const authRouter = Router();
 
 // Auth router
+authRouter.get("/checkAuth", authMiddleware, checkAuth);
 authRouter.post("/signUp", signUp);
-authRouter.get("/signIn", signIn);
+authRouter.post("/signIn", signIn);
+authRouter.post("/signOut", signOut);
 
 export default authRouter;
