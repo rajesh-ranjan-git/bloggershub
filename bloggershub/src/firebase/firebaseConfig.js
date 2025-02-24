d408fc30bf793881179b632b73c3faaeb34398d5
Rajesh <rajeshranjan8271@gmail.com>
2025-02-25 00:57:27 +0530

    Added firebase config

---
+++ bloggershub/src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_z_iAdEzTD523llLVb2tOl7jDKYRj8z4",
+  authDomain: "bloggershub-fb2bb.firebaseapp.com",
+  projectId: "bloggershub-fb2bb",
+  storageBucket: "bloggershub-fb2bb.firebasestorage.app",
+  messagingSenderId: "1065652219817",
+  appId: "1:1065652219817:web:5f504f7272eb0772e85eb8",
+};
+
+// Initialize Firebase
+const firebaseAuth = initializeApp(firebaseConfig);
