2ea591910ee8a34429a16153f396f46209c5e9fe
Rajesh <rajeshranjan8271@gmail.com>
2025-02-26 23:48:36 +0530

    Added shimmer for latest posts

--- bloggershub/src/components/latestPosts/latestPosts.jsx
+++ bloggershub/src/components/latestPosts/latestPosts.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import fetchLatestPostsService from "@/services/posts/fetchLatestPostsService";
 import BlogCard from "@/components/blogCard/blogCard";
+import CustomPostSkeleton from "../customLoaderComponents/customPostSkeleton";
 
 const LatestPosts = () => {
   const dispatch = useDispatch();
@@ -18,12 +19,27 @@ const LatestPosts = () => {
       <div className="px-10 py-8 border-b-4 border-blue-400 font-semibold text-5xl">
         Latest Posts
       </div>
-      <div className="flex md:flex-row flex-col flex-wrap justify-center items-center gap-10 py-10 w-full">
-        {isLoading && <div>Posts are loading...</div>}
-        {posts && posts.length > 0
-          ? posts.map((post) => <BlogCard post={post} key={post.id} />)
-          : null}
-      </div>
+      {isLoading && (
+        <div className="justify-center md:justify-between gap-10 grid md:grid-cols-2 xl:grid-cols-4 py-10 w-full">
+          <>
+            <CustomPostSkeleton />
+            <CustomPostSkeleton />
+            <CustomPostSkeleton />
+            <CustomPostSkeleton />
+          </>
+        </div>
+      )}
+      {posts && posts.length > 0 ? (
+        <div className="justify-center md:justify-between gap-10 grid md:grid-cols-2 xl:grid-cols-4 py-10 w-full">
+          {posts.map((post) => (
+            <BlogCard post={post} key={post.id} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center py-10 w-full h-1/2 text-2xl">
+          No posts available to show...
+        </div>
+      )}
     </div>
   );
 };
