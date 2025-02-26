3e8b5dd918cdd0e8a3af5c981368ed2f1356dd26
Rajesh <rajeshranjan8271@gmail.com>
2025-02-27 02:55:17 +0530

    Added banner image

--- bloggershub/src/components/banner/banner.jsx
+++ bloggershub/src/components/banner/banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
   return (
-    <div className="flex justify-center items-center bg-blue-400 border-b w-full h-[90vh] text-white">
+    <div className="flex justify-center items-center bg-[url(/images/banner.jpg)] bg-cover bg-no-repeat bg-center mt-16 border-b w-full h-[80vh] text-black">
       <p className="text-5xl text-center">Welcome to Blogger's Hub</p>
     </div>
   );
