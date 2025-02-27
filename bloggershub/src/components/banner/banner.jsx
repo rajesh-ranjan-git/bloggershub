210c04e9d86169521ae2ed622cbf1c236a262e0e
Rajesh <rajeshranjan8271@gmail.com>
2025-02-28 03:14:28 +0530

    Added logo

--- bloggershub/src/components/banner/banner.jsx
+++ bloggershub/src/components/banner/banner.jsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+
 const Banner = () => {
   return (
-    <div className="flex justify-center items-center bg-[url(/images/banner.jpg)] bg-cover bg-no-repeat bg-center mt-16 border-b w-full h-[30vh] md:h-[50vh] xl:h-[80vh] text-black">
+    <div className="flex flex-col justify-center items-center bg-[url(/images/banner.jpg)] bg-cover bg-no-repeat bg-center mt-16 border-b w-full h-[30vh] md:h-[50vh] xl:h-[80vh] text-black">
+      <Image src="/logoIcon.svg" alt="logo" width={100} height={100} />
       <p className="text-xl md:text-3xl xl:text-5xl text-center">
         Welcome to Blogger's Hub
       </p>
--- bloggershub/src/components/header/logo.jsx
+++ bloggershub/src/components/header/logo.jsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Link href="/" className="font-bold text-3xl">
-      Blogger's Hub
+    <Link href="/" className="flex items-center gap-1 font-bold text-3xl">
+      <Image src="/logoIcon.svg" alt="logo" width={40} height={40} />
+      <span>Blogger's Hub</span>
     </Link>
   );
 };
