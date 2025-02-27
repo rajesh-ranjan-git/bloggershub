fe4c3c9644b97a74e714535d72ed723702130973
Rajesh <rajeshranjan8271@gmail.com>
2025-02-28 02:43:11 +0530

    Improved banner responsiveness

--- bloggershub/src/components/banner/banner.jsx
+++ bloggershub/src/components/banner/banner.jsx
@@ -1,7 +1,7 @@
 const Banner = () => {
   return (
-    <div className="flex justify-center items-center bg-[url(/images/banner.jpg)] bg-cover bg-no-repeat bg-center mt-16 border-b w-full h-[30vh] md:h-[80vh] text-black">
-      <p className="text-xl md:text-5xl text-center">
+    <div className="flex justify-center items-center bg-[url(/images/banner.jpg)] bg-cover bg-no-repeat bg-center mt-16 border-b w-full h-[30vh] md:h-[50vh] xl:h-[80vh] text-black">
+      <p className="text-xl md:text-3xl xl:text-5xl text-center">
         Welcome to Blogger's Hub
       </p>
     </div>
